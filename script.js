63bf2cff200ac18cad121639ca3359f0b472b02f
Razib Saha <34422246+razibsaha@users.noreply.github.com>
2022-06-07 10:47:01 +0600

    old file added with correction

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
-document.getElementById('calculate').addEventListener('click', function() {
-    // withdraw amount
+document.getElementById("calculate").addEventListener("click", function () {
+  //Getting the value of Income input
+  const incomeInput = document.getElementById("income-input");
+  const incomeValue = incomeInput.value;
+  const incomeAmount = parseFloat(incomeValue);
 
-    const incomeInput = document.getElementById('income-input');
-    const incomeText = incomeInput.value;
-    const incomeAmount = parseFloat(incomeText);
-    
-    const foodInput = document.getElementById('food-input');
-    const foodText = foodInput.value;
-    const foodAmount = parseFloat(foodText);
+  //Getting the value of Food input
+  const foodInput = document.getElementById("food-input");
+  const foodValue = foodInput.value;
+  const foodAmount = parseFloat(foodValue);
 
-    const rentInput = document.getElementById('rent-input');
-    const rentText = rentInput.value;
-    const rentAmount = parseFloat(rentText);
+  //Getting the value of Rent input
+  const rentInput = document.getElementById("rent-input");
+  const rentValue = rentInput.value;
+  const rentAmount = parseFloat(rentValue);
 
-    const clothesInput = document.getElementById('clothes-input');
-    const clothesText = clothesInput.value;
-    const clothesAmount = parseFloat(clothesText);
+  //Getting the value of Clothes input
+  const clothesInput = document.getElementById("clothes-input");
+  const clothesText = clothesInput.value;
+  const clothesAmount = parseFloat(clothesText);
 
-   
+  //Linking with the div id to show total expense
+  const totalExpense = document.getElementById("total-expenses");
+
+  const newTotalExpense = foodAmount + rentAmount + clothesAmount;
+  console.log(newTotalExpense);
+
+  totalExpense.innerText = newTotalExpense;
+
+  const newBalance = document.getElementById("rem-balance");
+  const newBalanceAfterExpenses =
+    incomeAmount - (foodAmount + rentAmount + clothesAmount);
+
+  newBalance.innerText = newBalanceAfterExpenses;
+
+  const typedNumbers = document.getElementById("failed").innerText;
+
+  if (incomeValue != "number") {
+    typedNumbers.style.display = "block";
+  } else if (input == "number") {
+    typedNumbers.style.display = "hidden";
+  }
+});
+
+document.getElementById("savings").addEventListener("click", function () {
+  const incomeInput = document.getElementById("income-input");
+  const incomeValue = incomeInput.value;
+  const incomeAmount = parseFloat(incomeValue);
 
 
-    
+  const savingsInput = document.getElementById("save-input");
+  const saveValue = savingsInput.value;
+  const saveAmount = parseFloat(saveValue);
 
-    // const previousWithdrawText = withdrawTotal.innerText;
-    // const previousWithdrawAmount = parseFloat(previousWithdrawText);
-    // const newWithdrawTotal = incomeAmount - (foodAmount+rentAmount+clothesAmount) 
-    // withdrawTotal.innerText = newWithdrawTotal;
+  const savingAmount = saveAmount * (incomeAmount / 100);
+  const getSavingAmount = document.getElementById("saving-amount");
+  getSavingAmount.innerHTML = savingAmount.toFixed(2);
 
-    //clear
-
-    // withdrawInput.value = '';
-
-    // Update accounat balance
-    
-
-    const totalExpense = document.getElementById('total-expenses');
-    // depositTotal.innerText = depositAmount;
-
-    const previousTotalExpenseText = totalExpense.value;
-    const previousTotalExpenseAmount = parseFloat(previousTotalExpenseText);
-    const newTotalExpense = foodAmount+rentAmount+clothesAmount;
-
-    totalExpense.innerText = newTotalExpense;
-    
-    const newBalance = document.getElementById('balance');
-    
-    const newBalanceAfterExpenses = incomeAmount - (foodAmount+rentAmount+clothesAmount);
-
-    newBalance.innerText = newBalanceAfterExpenses;
-
-    
-    
-    
-    const typedNumbers = document.getElementById('failed').innerText;
-    
-    if (incomeText != 'number'){
-        typedNumbers.style.display = 'block';
-
-    } else if(input == 'number'){
-            typedNumbers.style.display = 'hidden';
-    }
-    
-})
+  const remainingBalance = document.getElementById('remaining-amount')
+  const remainingAmount = incomeAmount - savingAmount;
+  remainingBalance.innerHTML = remainingAmount.toFixed(2);
+});
